0d1eecef3cfc544a79d8ad0251f77c13029d3f6a
Suraj Kumar <108967267+Suraj930@users.noreply.github.com>
2022-10-08 21:49:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Instagram</h1>
+    <p data-ns-test="project-description">Instagreen Would be a instagreen clone app which have all the functinality of instagram using firebase as a database</p>
+    </div>
   )
 }
 
